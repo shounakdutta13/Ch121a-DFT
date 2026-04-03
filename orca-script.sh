01eeb657a3172ae12a2f66053b9c7c365bdbe7e8
Prabhat Prakash <pptkavi@gmail.com>
2026-04-03 15:18:25 -0700

    Update orca-script.sh

--- orca-script.sh
+++ orca-script.sh
@@ -2,12 +2,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --time=0:30:00               # Time limit hrs:min:sec
-module load openmpi/4.1.2-gcc-11.3.1-7kosie3
+module load orca/6.1.0 
 pwd
 cwd=$(pwd)
 echo $SLURM_JOBID "${cwd}" >> ~/job_history
 
-/resnick/groups/wag/prabhat/programs/orca-6.0.0/orca_6_0_0/orca test.inp > test.out
+/central/software9/external/orca/orca_6_1_0_linux_x86-64_shared_openmpi418_avx2/orca test.inp > test.out
 
 
 date
+
